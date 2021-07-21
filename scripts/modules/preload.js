7c73f00f2ae88f31cb1e504b153316f9d5ae5314
Philip Sole <philip.sole@gmail.com>
2021-07-21 22:08:01 +1200

    data text for variable display. Background image. Camera follow.

--- scripts/modules/preload.js
+++ scripts/modules/preload.js
@@ -1,6 +1,6 @@
 FD.preload = function() {
     console.log('preload');
 
-    this.load.setBaseURL('/assets/');
+    this.load.setBaseURL('./assets/');
     this.load.image('water', 'backgrounds/underwater.jpg');
 }
